276316ef0bb550d08a9a07669fce83c0fd37a7bc
Naman Kumar Singh <114607379+namansingh1111@users.noreply.github.com>
2023-11-08 01:26:28 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -17,55 +17,55 @@ function App() {
           <tr>
             <td>my-react-app</td>
             <td>
-              <a href="https://github.com/hritsk/Task3_my_react_app">Link 1</a>
+              <a href="https://github.com/namansingh1111/Task3_my_react_app-">Link 1</a>
             </td>
             <td>
-              <a href="https://task3-my-react-app-vercel.vercel.app/">Link 1</a>
+              <a href="https://task3-my-react-app-six.vercel.app/">Link 1</a>
             </td>
           </tr>
           <tr>
             <td>react-calculator-app</td>
             <td>
-              <a href="https://github.com/hritsk/Task3_calculator">Link 2</a>
+              <a href="https://github.com/namansingh1111/Task3_calculator-">Link 2</a>
             </td>
             <td>
-              <a href="https://task3-calculator-vercel.vercel.app/">Link 2</a>
+              <a href="https://task3-my-react-app-el62.vercel.app/">Link 2</a>
             </td>
           </tr>
           <tr>
             <td>color-picker-react-app</td>
             <td>
-              <a href="https://github.com/hritsk/Task3_color_picker">Link 3</a>
+              <a href="https://github.com/namansingh1111/Task3_color_picker-">Link 3</a>
             </td>
             <td>
-              <a href="https://task3-color-picker-vercel.vercel.app/">Link 3</a>
+              <a href="https://task3-color-picker-steel.vercel.app/">Link 3</a>
             </td>
           </tr>
           <tr>
             <td>usertable-react-app</td>
             <td>
-              <a href="https://github.com/hritsk/Task3_Usertable_react_app">Link 4</a>
+              <a href="https://github.com/namansingh1111/Task3_Usertable_react_app">Link 4</a>
             </td>
             <td>
-              <a href="https://task3-usertable-react-app-vercel.vercel.app/">Link 4</a>
+              <a href="https://task3-usertable-react-app-two.vercel.app/">Link 4</a>
             </td>
           </tr>
           <tr>
             <td>age-calculator-react-app</td>
             <td>
-              <a href="https://github.com/hritsk/Task3_age_calculator">Link 5</a>
+              <a href="https://github.com/namansingh1111/Task3_age_calculator">Link 5</a>
             </td>
             <td>
-              <a href="https://task3-age-calculator-vercel.vercel.app/">Link 5</a>
+              <a href="https://task3-age-calculator-three.vercel.app/">Link 5</a>
             </td>
           </tr>
           <tr>
             <td>portfolio</td>
             <td>
-              <a href="https://github.com/hritsk/Task3_Portfolio_react">Link 6</a>
+              <a href="https://github.com/namansingh1111/Task3_Portfolio_react-">Link 6</a>
             </td>
             <td>
-              <a href="https://task3-portfolio-react-six.vercel.app/">Link 6</a>
+              <a href="https://task3-portfolio-react-five.vercel.app/">Link 6</a>
             </td>
           </tr>
         </tbody>
